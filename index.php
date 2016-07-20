3fedd5cce443a4dddee45e91c7869806cffb7c7a
bezmian <filip@norvox.pl>
2016-07-20 23:12:36 +0200

    index page added

--- index.php
+++ index.php
@@ -1,53 +1,36 @@
 <html>
+    <head>
+        <meta charset="utf-8">
+        <link href="css/stylesheet.css" rel="stylesheet" media="screen, projection"/>
+        <title>kaja seweryn</title>
+    </head>
+    <body>
+        <div class="content_centered">
+            <div class="header">
+                kaja seweryn
+                <hr/>
+            </div>
 
-  <head>
-    <meta charset="utf-8">
-    <link href="css/stylesheet.css" rel="stylesheet" media="screen, projection"/>
-    <title>kaja seweryn</title>
-    <script src="https://use.typekit.net/iex0ren.js"></script>
-<script>try{Typekit.load({ async: true });}catch(e){}</script>
-  </head>
+            <div class="row">
+                <div class="menu_btn">
+                    <img src="images/menu/digital.jpg"/>
+                </div>
+                <div class="menu_btn">
+                    <img src="images/menu/traditional.jpg"/>
+                </div>
+                <div class="menu_btn">
+                    <img src="images/menu/info.jpg"/>
+                </div>
+            </div>
+            <hr/>
 
-  <body>
-
-    <div class="content">
-
-      <div class="header">
-        <h1>kaja seweryn</h1>
-      </div>
-
-    <hr/>
-
-      <div class="fotos">
-
-        <div class="square">
-          <a href="art.php"><img src="../images/all/art.JPG" alt="art"></a>
+            <div class="footer_note">
+                <p> Hello! I'm Kaja, 19 years old graphic design student. I was born in Poland,
+                    in Cracow, where I spent the major part of my life. In September 2015 I moved
+                    to Berlin, where I started my studies in "Kommunikationsdesign" at the University
+                    of Applied Sciences (Hochschule für Technik und Wirtschaft).
+                </p>
+            </div>
         </div>
-
-        <div class="square">
-          <a href="photos.php"><img src="../images/all/photos.JPG" alt="photos"></a>
-        </div>
-
-        <div class="square">
-          <a href="info.php"><img src="../images/all/info.JPG" alt="info"></a>
-        </div>
-
-      </div>
-    </div>
-
-    <hr/>
-
-    <div class="tekst">
-    <p> Hello! I'm Kaja, 19 years old graphic design student. I was born in Poland,
-    in Cracow, where I spent the major part of my life. In September 2015 I moved
-    to Berlin, where I started my studies in "Kommunikationsdesign" at the University
-    of Applied Sciences (Hochschule für Technik und Wirtschaft).</p>
-    </div>
-
-    <div class="logo">
-      <p>ks</p>
-    </div>
-
-
-  </body>
+    </body>
 </html>
